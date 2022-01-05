977a598e42b892223c2cea8bf391642129c5a4f8
pskfyi <patrick.keenan.fyi@gmail.com>
2022-01-05 11:19:51 -0800

    lint: format

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <template>
   <div class="gridcontainer container mx-auto bg-red-500">
-    <header>
-        I'm a header. Check this box
-    </header>
-    <TabNav/>
-    <footer>
-        This is a game jam game.
-    </footer>
+    <header>I'm a header. Check this box</header>
+    <TabNav />
+    <footer>This is a game jam game.</footer>
   </div>
 </template>
 
@@ -18,19 +14,18 @@ export default {
 
 <style scoped>
 .gridcontainer {
-    display: grid | inline-grid;
+  display: grid;
 }
 
 header {
-    grid-row: 1;
+  grid-row: 1;
 }
 
 .gridcenter {
-    grid-row: 2;
+  grid-row: 2;
 }
 
 footer {
-    grid-row: 3;
+  grid-row: 3;
 }
-
 </style>
