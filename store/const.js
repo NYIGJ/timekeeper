5688d965f9b2dbfa86adf89eee1a6e5d1c4e4816
John McCardle <mccardle.john@gmail.com>
2022-01-06 18:43:12 -0500

    import const currencies as object properly

--- store/const.js
+++ store/const.js
@@ -1,12 +1,10 @@
-export default {
-  currencies: {
-    energy: 'energy',
-    seasons: 'seasons',
-    months: 'months',
-    days: 'days',
-    hours: 'hours',
-    seconds: 'seconds',
-    nanoseconds: 'nanoseconds',
-    plancktime: 'plancktime',
-  },
+export const currencies = {
+  energy: 'energy',
+  seasons: 'seasons',
+  months: 'months',
+  days: 'days',
+  hours: 'hours',
+  seconds: 'seconds',
+  nanoseconds: 'nanoseconds',
+  plancktime: 'plancktime',
 }
