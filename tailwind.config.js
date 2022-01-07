d56b1a406ee741763cafbf9dbb008e4f271f4a12
pskfyi <patrick.keenan.fyi@gmail.com>
2022-01-06 22:29:10 -0800

    chore: always publish colors
    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 const colors = require('tailwindcss/colors')
 
+const { black, white, ...rainbow } = colors
+
+const COLOR_TYPE = ['bg', 'text', 'border']
+const COLOR_NAME = Object.keys(rainbow)
+const COLOR_NUMBER = Object.keys(rainbow.rose) // any color will do
+
+const safelist = COLOR_TYPE.flatMap((type) =>
+  COLOR_NAME.flatMap((name) =>
+    COLOR_NUMBER.map((number) => `${type}-${name}-${number}`)
+  )
+)
+
 module.exports = {
-  purge: [],
+  purge: {
+    safelist,
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
