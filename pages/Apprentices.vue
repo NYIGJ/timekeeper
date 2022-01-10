3684b4b223bd5d40e541f8ac4d9306a2565a8117
pskfyi <patrick.keenan.fyi@gmail.com>
2022-01-10 02:14:44 -0800

    feat: appretices tab draft 2

--- pages/Apprentices.vue
+++ pages/Apprentices.vue
@@ -1,31 +1,9 @@
 <template>
-  <div class="tab-content grid flex-col">
-    <div
+  <responsive-grid>
+    <apprentice-button
       v-for="(process, index) in $store.state.processes"
-      :key="index"
-      class="flex flex-col mb-4 p-4 rounded border"
-      :class="`border-${$store.getters.activeTab.darkColor}`"
-      @click="$store.commit('purchaseWorker', index)"
-    >
-      <span class="text-center font-bold pb-2 text-xl">
-        {{ process.workerCount }} {{ process.worker }}
-        <template v-if="process.workerCount != 1">s</template>
-      </span>
-      <span class="text-left font-semibold">
-        Each <b>{{ process.worker }}</b> makes your
-        <b>{{ process.device }}s</b> produce <b>spare time</b> at an increased
-        rate.
-      </span>
-      <span class="text-center text-md pt-2">
-        Cost for next {{ process.worker }}: {{ process.nextWorkerCost }}
-        {{ process.buyWorkersWith }}
-      </span>
-    </div>
-  </div>
+      :key="`${index}-${process.workerLevel}`"
+      :process="process"
+    />
+  </responsive-grid>
 </template>
-
-<style>
-.tab-content {
-  grid-template-columns: 1fr;
-}
-</style>
