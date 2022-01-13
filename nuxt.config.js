14f2fbb7b5ad1b3a22fb63f85159ad45302df19f
pskfyi <patrick.keenan.fyi@gmail.com>
2022-01-12 18:56:31 -0800

    fix: itch sucks

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,8 +75,10 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     publicPath: './_nuxt/',
-    extend(config) {
-      config.output.publicPath = './_nuxt/'
+    extend(config, { isDev }) {
+      if (!isDev) {
+        config.output.publicPath = './_nuxt/'
+      }
     },
   },
 
