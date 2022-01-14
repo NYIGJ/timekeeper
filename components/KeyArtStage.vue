45fc84b3dbccfdd41fd0e0aaafd901882daa527c
pskfyi <patrick.keenan.fyi@gmail.com>
2022-01-13 19:57:35 -0800

    chore: messaging about spare time, energy, and mana

--- components/KeyArtStage.vue
+++ components/KeyArtStage.vue
@@ -2,18 +2,22 @@
   <div class="p-8 relative">
     <div class="key-art absolute top-8 left-0 right-0" />
     <div
-      class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center"
+      class="absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center"
+      :class="`text-${$store.getters.activeTab.darkColor}`"
       @click="click"
     >
       <span
         class="spare-time flex flex-row items-center font-bold rounded-2xl select-none"
-        :class="`text-${$store.getters.activeTab.darkColor}`"
       >
         <span
           class="spare-time-value text-3xl md:text-5xl"
           v-text="currencyText"
         />
         <span class="fas fa-hourglass-half text-xl pl-2 md:text-3xl md:pt-1" />
+      </span>
+      <span class="spare-time-explanation text-sm md:text-lg select-none">
+        Tap to gain
+        <b><span class="fas fa-hourglass-half" /> Spare Time</b>
       </span>
     </div>
   </div>
@@ -54,8 +58,14 @@ export default {
 }
 .spare-time {
   --color: rgba(2555, 255, 255, 0.5);
-  background: var(--color);
   box-shadow: 0px 0px 20px 20px var(--color);
+  background: var(--color);
+  transition: color 2000ms;
+}
+.spare-time-explanation {
+  --color: rgba(2555, 255, 255, 0.2);
+  box-shadow: 0px 0px 10px 10px var(--color);
+  background: var(--color);
   transition: color 2000ms;
 }
 </style>
