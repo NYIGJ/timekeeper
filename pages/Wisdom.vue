0457a5d7ecf64d683018e0ec6dac9fe733fa955b
John McCardle <mccardle.john@gmail.com>
2022-01-14 21:22:16 -0500

    feat: track various stats and display on Wisdom tab

--- pages/Wisdom.vue
+++ pages/Wisdom.vue
@@ -12,25 +12,25 @@
       }}</span>
 
       <span align="left"><b>Total in-game time elapsed</b></span
-      ><span align="right"></span>
+      ><span align="right">{{ $store.getters.gameTimeTotalText }}</span>
 
       <span align="left"><b>Completed lifetimes</b></span
       ><span align="right">{{ $store.state.lifetimes }}</span>
 
-      <span align="left"><b>Longest lifetime</b></span
-      ><span align="right"></span>
-
       <span align="left"><b>Oldest age reached</b></span
-      ><span align="right"></span>
+      ><span align="right">{{ $store.getters.ageRecordText }}</span>
 
       <span align="left"><b>Latest time reached</b></span
-      ><span align="right"></span>
+      ><span align="right">{{ $store.getters.gameDateRecordText }}</span>
 
       <span align="left"><b>Wisdom for next prestige</b></span
       ><span align="right">{{ $store.state.wisdomGained }}</span>
 
       <span align="left"><b>Wisdom from other timelines</b></span
-      ><span align="right">{{ $store.state.wisdomApplied }}</span>
+      ><span align="right"
+        >{{ $store.state.wisdomApplied }}: +{{ $store.state.wisdomApplied }}% to
+        Spare Time, Energy, and Mana gained</span
+      >
     </div>
     <hr />
 
